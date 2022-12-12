89ce720697c4e284446a809c8d5620747782d37e
Akshay Khona <DesertSled800c@gmail.com>
2022-12-12 10:31:31 -0600

    proj complete

---
+++ main.js
@@ -0,0 +1,14 @@
+//  putting marbles into bags
+const { createMarble } = require("./marbleMaker.js")
+
+const marble = createMarble("small")
+console.log(marble)
+
+
+
+
+
+const { weaveBag } = require("./bagMaker.js")
+
+const bag = weaveBag("silk")
+console.log(bag)
